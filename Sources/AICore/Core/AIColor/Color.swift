0ad1c272e9e6845ec39320d3ca2e030b415c13b3
Alexy <alexy.ib@outlook.com>
2023-02-25 13:55:31 +0200

    wip

---
+++ Sources/AICore/Core/AIColor/Color.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexy Ibrahim on 2/25/23.
+//
+
+import Foundation
+
+public extension AIColor {
+    static var pureBlack: AIColor { ThemeManager.shared.current.color.pureBlack }
+    static var pureWhite: AIColor { ThemeManager.shared.current.color.pureWhite }
+    static var blackWhite: AIColor { ThemeManager.shared.current.color.blackWhite }
+    static var whiteBlack: AIColor { ThemeManager.shared.current.color.whiteBlack }
+    static var error: AIColor { ThemeManager.shared.current.color.error }
+    static var success: AIColor { ThemeManager.shared.current.color.success }
+    static var clear: AIColor { ThemeManager.shared.current.color.clear }
+}
--- Sources/AICore/UIComponents/Theme/MainTheme/MainColorTheme.swift
+++ Sources/AICore/UIComponents/Theme/MainTheme/MainColorTheme.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 public class MainColorTheme {
-    public var black: AIColor {AIColor.init(.black, dark: .white)}
-    public var white: AIColor {AIColor.init(.white, dark: .black)}
+    public var pureBlack: AIColor {AIColor.init(.black)}
+    public var pureWhite: AIColor {AIColor.init(.white)}
+    public var blackWhite: AIColor {AIColor.init(.black, dark: .white)}
+    public var whiteBlack: AIColor {AIColor.init(.white, dark: .black)}
+    public var error: AIColor {AIColor.init(.systemRed)}
+    public var success: AIColor {AIColor.init(.systemGreen)}
     public var clear: AIColor {AIColor.init(.clear, dark: .clear)}
 }
