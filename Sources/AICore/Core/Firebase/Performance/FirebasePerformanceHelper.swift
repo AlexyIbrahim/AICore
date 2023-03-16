ff0da5d07887b5ea996c5953442ee929af29c013
Alexy <alexy.ib@outlook.com>
2023-03-16 13:43:25 +0200

    wip

--- Sources/AICore/Core/Firebase/Performance/FirebasePerformanceHelper.swift
+++ Sources/AICore/Core/Firebase/Performance/FirebasePerformanceHelper.swift
@@ -25,4 +25,28 @@ public class FirebasePerformanceHelper {
     public class func incrementMetric(forTrace trace: FirebasePerformance.Trace, metric: String, by: Int64? = nil) {
         trace.incrementMetric(metric, by: by ?? 1)
     }
+    
+    public class func valueForMetric(forTrace trace: FirebasePerformance.Trace, metric: String) -> Int64 {
+        trace.valueForMetric(metric)
+    }
+    
+    public class func valueForAttribute(forTrace trace: FirebasePerformance.Trace, attribute: String) -> String? {
+        trace.value(forAttribute: attribute)
+    }
+    
+    public class func removeAttribute(forTrace trace: FirebasePerformance.Trace, attribute: String) {
+        trace.removeAttribute(attribute)
+    }
+    
+    public class func attributes(forTrace trace: FirebasePerformance.Trace) -> [String: String] {
+        trace.attributes
+    }
+    
+    public class func setMetric(forTrace trace: FirebasePerformance.Trace, metric: String, value: Int64) {
+        trace.setValue(value, forMetric: metric)
+    }
+    
+    public class func setAttribute(forTrace trace: FirebasePerformance.Trace, attribute: String, value: String) {
+        trace.setValue(value, forAttribute: attribute)
+    }
 }
