692c6849872e41017b72814df38f2707856a3292
Alexy <alexy.ib@outlook.com>
2023-04-27 18:15:51 +0300

    wip

--- Sources/AICore/Core/Utils/Public.swift
+++ Sources/AICore/Core/Utils/Public.swift
@@ -11,14 +11,16 @@ import AIEnvironmentKit
 public func print(_ items: String..., filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n", sameLine: Bool? = nil) {
     AIEnvironmentKit.executeIfNotAppStore {
         let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\((sameLine ?? true) ? "" : "\n")\t-> "
-        let output: String = items.map { "\($0)" }.joined(separator: separator)
+        let output = items.map { "\($0)" }.joined(separator: separator)
         if output.isNotEmpty {
+            let final_print: String = pretty+output
             if Config.LOG_PRINTS {
                 Utils.writeTextToFile("• \(final_print)", fileName: Config.log_file_name, folderName: Config.log_folder_name)
                 DebugHelper.log(final_print)
             }
-            Swift.print(final_print, terminator: terminator)
-            Utils.logs_updated.send()
+            if final
+                Swift.print(final_print, terminator: terminator)
+                Utils.logs_updated.send()
         }
     }
 //#if DEBUG
