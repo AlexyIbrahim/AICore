341397d37e63bf5cd7ffb21e454cc86f92744889
Alexy <alexy.ib@outlook.com>
2023-04-27 18:14:34 +0300

    wip

--- Sources/AICore/Core/Utils/Public.swift
+++ Sources/AICore/Core/Utils/Public.swift
@@ -11,14 +11,15 @@ import AIEnvironmentKit
 public func print(_ items: String..., filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n", sameLine: Bool? = nil) {
     AIEnvironmentKit.executeIfNotAppStore {
         let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\((sameLine ?? true) ? "" : "\n")\t-> "
-        let output = items.map { "\($0)" }.joined(separator: separator)
-        let final_print: String = pretty+output
-        if Config.LOG_PRINTS {
-            Utils.writeTextToFile("• \(final_print)", fileName: Config.log_file_name, folderName: Config.log_folder_name)
-            DebugHelper.log(final_print)
+        let output: String = items.map { "\($0)" }.joined(separator: separator)
+        if output.isNotEmpty {
+            if Config.LOG_PRINTS {
+                Utils.writeTextToFile("• \(final_print)", fileName: Config.log_file_name, folderName: Config.log_folder_name)
+                DebugHelper.log(final_print)
+            }
+            Swift.print(final_print, terminator: terminator)
+            Utils.logs_updated.send()
         }
-        Swift.print(final_print, terminator: terminator)
-        Utils.logs_updated.send()
     }
 //#if DEBUG
 //#else
@@ -29,12 +30,14 @@ public func print(_ items: String..., filename: String = #file, function : Strin
 public func print(_ items: Any...,  separator: String = " ", terminator: String = "\n") {
     AIEnvironmentKit.executeIfNotAppStore {
         let output = items.map { "\($0)" }.joined(separator: separator)
-        if Config.LOG_PRINTS {
-            Utils.writeTextToFile("• \(output)", fileName: Config.log_file_name, folderName: Config.log_folder_name)
-            DebugHelper.log(output)
+        if output.isNotEmpty {
+            if Config.LOG_PRINTS {
+                Utils.writeTextToFile("• \(output)", fileName: Config.log_file_name, folderName: Config.log_folder_name)
+                DebugHelper.log(output)
+            }
+            Swift.print(output, terminator: terminator)
+            Utils.logs_updated.send()
         }
-        Swift.print(output, terminator: terminator)
-        Utils.logs_updated.send()
     }
 //#if DEBUG
 //#else
