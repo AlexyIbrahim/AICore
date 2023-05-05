59fdfd5d22b056c230847b98ea852dbe19c307a4
Alexy <alexy.ib@outlook.com>
2023-05-05 19:28:07 +0300

    wip

---
+++ Sources/AICore/Core/Extensions/BoolExtension.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexy Ibrahim on 5/5/23.
+//
+
+import Foundation
+
+public extension Bool {
+    var isFalse: Bool {
+        self == false
+    }
+    
+    var isTrue: Bool {
+        self == true
+    }
+}
