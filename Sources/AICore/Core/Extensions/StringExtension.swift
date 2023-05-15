803999790b762cd1fcea070351c2b49106b496e8
Alexy <alexy.ib@outlook.com>
2023-05-15 15:31:07 +0300

    wip

--- Sources/AICore/Core/Extensions/StringExtension.swift
+++ Sources/AICore/Core/Extensions/StringExtension.swift
@@ -76,7 +76,7 @@ public extension String {
         return ceil(boundingBox.width)
     }
     
-    func isOnlyAlphabetical() -> Bool {
+    var isOnlyAlphabetical -> Bool {
         return self.allSatisfy { $0.isLetter }
 
 //        let letters = CharacterSet.letters
