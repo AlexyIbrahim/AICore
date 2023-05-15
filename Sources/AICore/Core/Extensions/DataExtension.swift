b8d364efef68c60c9390e1343cf451adc805160b
Alexy <alexy.ib@outlook.com>
2023-05-15 14:31:42 +0300

    wip

---
+++ Sources/AICore/Core/Extensions/DataExtension.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexy Ibrahim on 5/15/23.
+//
+
+import Foundation
+import UIKit
+
+public extension Data {
+    var image: UIImage? {
+        if let image = UIImage(data: self) {
+            return image
+        }
+        return nil
+    }
+}
