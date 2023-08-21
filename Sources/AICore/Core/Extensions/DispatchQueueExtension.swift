733b07cd3e789c6724a64ab9983bae00a24ed2c9
Alexy <alexy.ib@outlook.com>
2023-08-22 00:07:47 +0300

    Refactor DispatchQueueExtension.swift for better code organization and access control
    - Made the `mainThread` and `backgroundThread` functions public for easier access from other modules.
    - Added parameter labels to improve readability.
    - Updated comments to reflect changes.
    
    This commit improves the code organization and access control in DispatchQueueExtension.swift. The `mainThread` and `backgroundThread` functions are now public, allowing easier access from other modules. Additionally, parameter labels have been added to improve readability.

--- Sources/AICore/Core/Extensions/DispatchQueueExtension.swift
+++ Sources/AICore/Core/Extensions/DispatchQueueExtension.swift
@@ -37,17 +37,17 @@ public extension DispatchQueue {
 /// - Parameters:
 ///   - code: The code to run on the main thread.
 ///   - delay: The time in seconds before the code should start. Default is nil, which means it starts immediately.
-func mainThread(_ code: @escaping (() -> Void), delay: Double? = nil) {
+public func mainThread(_ code: @escaping (() -> Void), delay: Double? = nil) {
 	DispatchQueue.main(code, delay: delay)
 }
 
 /// Function to run code in the background thread.
 /// Optionally, you can specify a delay and a completion block to run on the main thread after the background code is done executing.
 /// - Parameters:
-///   - background: The code to run in the background.
+///   -p background: The code to run in the background.
 ///   - delay: The time in seconds before the background code should start. Default is nil, which means it starts immediately.
 ///   - completionOnMain: The code to run on the main thread after the background code is done. Default is nil, which means nothing is run afterwards.
 ///   - completionDelay: The time in seconds before the completion code should start. Default is nil, which means it starts immediately after the background code is done.
-func backgroundThread(background: @escaping (() -> Void), delay: Double? = nil, completionOnMain: (() -> Void)? = nil, completionDelay: Double? = nil) {
+public func backgroundThread(background: @escaping (() -> Void), delay: Double? = nil, completionOnMain: (() -> Void)? = nil, completionDelay: Double? = nil) {
 	DispatchQueue.background(background: background, delay: delay, completionOnMain: completionOnMain, completionDelay: completionDelay)
 }
