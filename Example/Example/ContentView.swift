ccf9567123205d186ab6a9503939b2f7194c8437
Alexy <alexy.ib@outlook.com>
2023-08-28 19:41:59 +0300

    wip

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Alexy Ibrahim on 8/28/23.
+//
+
+import SwiftUI
+import AICore
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+			Button("in app notification", role: .none) {
+				Utils.scheduleNotification(title: "Activate PTT", body: "Click here to activate PTT", delaySeconds: 1)
+			}
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Alexy Ibrahim on 8/28/23.
+//
+
+import SwiftUI
+import AICore
+
+@main
+struct ExampleApp: App {
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
