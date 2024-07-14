2c8f883e05c32950d389646a585791210600a91e
Alexy <alexy.ib@outlook.com>
2024-07-14 15:51:30 +0300

    Bump version: 2.4.53 --> 2.4.54

---
+++ Sources/AICore/Core/Extensions/UISheetPresentationControllerDetentExtension.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+@available(iOS 16.0, *)
+public extension UISheetPresentationController.Detent {
+	
+	// Convenience method for creating a detent with a fixed height
+	static func custom(_ height: CGFloat) -> UISheetPresentationController.Detent {
+		return self.custom { _ in
+			return height
+		}
+	}
+	
+	// Convenience method for creating a detent with a percentage of screen height
+	static func percentage(_ percentage: CGFloat) -> UISheetPresentationController.Detent {
+		return self.custom { context in
+			return context.maximumDetentValue * percentage
+		}
+	}
+}
+
