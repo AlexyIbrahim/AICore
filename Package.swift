f85ce7f8386e665f77422fe6fdd6e31d10272be5
Alexy <alexy.ib@outlook.com>
2024-07-14 13:58:14 +0300

    Bump version: 2.4.51 --> 2.4.52

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// Package version: 2.4.51
+// Package version: 2.4.52
 
 import PackageDescription
 
