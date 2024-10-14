7317c05b101001f00321f76d9c01a60f5069ddf9
Alexy <alexy.ib@gmail.com>
2024-10-14 23:14:05 +0300

    Bump version: 2.4.81 --> 2.4.82

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// Package version: 2.4.81
+// Package version: 2.4.82
 
 import PackageDescription
 
@@ -20,18 +20,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://gitlab.com/alexyib/ainetworkcalls.git", from: "1.5.15"),
-        .package(url: "https://github.com/relatedcode/ProgressHUD.git", from: "13.0.0"),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0"),
+        .package(url: "https://gitlab.com/alexyib/ainetworkcalls.git", from: "1.5.18"),
+        .package(url: "https://github.com/relatedcode/ProgressHUD.git", from: "14.1.3"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
         .package(url: "https://gitlab.com/alexyib/aienvironmentkit.git", from: "1.0.0"),
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.0.0"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.0.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.0.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.5.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.37.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.3.0"),
         .package(url: "https://github.com/malcommac/SwiftDate.git", from: "7.0.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
-        .package(url: "https://github.com/google/promises.git", from: "2.0.0"),
-        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "16.1.0"),
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.1.0"),
+        .package(url: "https://github.com/google/promises.git", from: "2.3.1"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "16.1.1"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -45,13 +45,13 @@ let package = Package(
                 .product(name: "ProgressHUD", package: "ProgressHUD"),
                 .product(name: "Lottie", package: "lottie-ios"),
                 .product(name: "Sentry", package: "sentry-cocoa"),
-                .product(name: "FirebaseAnalyticsSwift", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
                 .product(name: "FirebasePerformance", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseRemoteConfigSwift", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
                 .product(name: "SwiftDate", package: "SwiftDate"),
                 .product(name: "Kingfisher", package: "Kingfisher"),
                 .product(name: "Promises", package: "promises"),
